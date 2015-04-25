738aef062d94c11b7986afdda3928bf7dbd2ec44
Hemstreet <jonhemstreet@gmail.com>
2015-04-24 22:13:45 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+    // Let's scrape Anchorman 2
+    var url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+
+            var title, release, rating;
+            var json = { title : "", release : "", rating : ""};
+
+            $('.header').filter(function(){
+                var data = $(this);
+                title = data.children().first().text();
+                release = data.children().last().children().text();
+
+                json.title = title;
+                json.release = release;
+            })
+
+            $('.star-box-giga-star').filter(function(){
+                var data = $(this);
+                rating = data.text();
+
+                json.rating = rating;
+            })
+        }
+
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+            console.log('File successfully written! - Check your project directory for the output.json file');
+        });
+
+        res.send('\nCheck your console!\n\n')
+    });
+});
+
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
